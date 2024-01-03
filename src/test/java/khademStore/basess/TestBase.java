7d8e3c5a07f152b476aa8c8e1cb4271d63942649
Ahmed Abdelaziz <134834573+Ahmed-9595@users.noreply.github.com>
2024-01-03 14:57:52 +0200

    update with new mail approch

--- src/test/java/khademStore/basess/TestBase.java
+++ src/test/java/khademStore/basess/TestBase.java
@@ -1,4 +1,9 @@
 package khademStore.basess;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.qameta.allure.Allure;
 import khademStore.factoryPage.Factory;
 import org.apache.commons.io.FileUtils;
@@ -8,38 +13,65 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestBase {
-    protected WebDriver driver ;
-    public void driver(WebDriver driver){
-        this.driver=driver;
-    }
+    protected WebDriver driver;
+    private ExtentReports extent;
+    private ExtentTest test;
+
     @BeforeMethod
     public void setup() {
         driver = new Factory().initialization();
+
+        // Set up Extent Report
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("test-output/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
     }
+
     @AfterMethod
-    public void tearDown(ITestResult result){
-        String testResult= result.getMethod().getMethodName();
-        String fileName = "target"+File.separator+"screenshots"+File.separator+testResult+".png";
+    public void tearDown(ITestResult result) {
+        String testResult = result.getMethod().getMethodName();
+        String fileName = "target/screenshots/" + testResult + ".png";
         captureScreenShot(fileName);
-       driver.quit();
+        // Log test status in Extent Report
+        test = extent.createTest(testResult);
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, "Test failed");
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, "Test passed");
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            test.log(Status.SKIP, "Test skipped");
+        }
+
+        // Attach screenshot to Extent Report
+        try {
+            test.addScreenCaptureFromPath(fileName);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // Flush Extent Report
+        extent.flush();
+        driver.quit();
     }
-    public void captureScreenShot(String fileName){
-        File screenShot= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+    public void captureScreenShot(String fileName) {
+        File screenShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
             FileUtils.copyFile(screenShot, new File(fileName));
         } catch (IOException e) {
             throw new RuntimeException(e);
-    }
-        InputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(fileName);
-        } catch (FileNotFoundException e) {
+        }
+
+        try (InputStream inputStream = new FileInputStream(fileName)) {
+            Allure.addAttachment("ScreenShots", inputStream);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Allure.addAttachment("screenShots",inputStream);
-}
-
     }
+}
